e91ffe96a3ac99ff362a921afb3aaa403196a8ac
MorneNemdil <mornenemdilbusiness@gmail.com>
2025-06-18 12:46:01 +0100

    work section complete

--- src/components/work-carousel-mobile.tsx
+++ src/components/work-carousel-mobile.tsx
@@ -1,6 +1,20 @@
-const WorkCarouselMobile = () => {
+import type { JSX } from "react";
+import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
+import { Card } from "./ui/card";
+
+type WorkCarouselMobileProps = {
+    slides: { index: number, content: JSX.Element }[];
+}
+
+const WorkCarouselMobile = ({ slides }: WorkCarouselMobileProps) => {
     return (
-        <div>WorkCarouselMobile</div>
+        <Carousel className="w-[70vw]">
+            <CarouselContent>
+                {slides.map(slide => <CarouselItem className="!h-[50vh] md:!h-[60vh] lg:!h-[70vh]" key={slide.index}>
+                    <Card className="h-full">{slide.content}</Card>
+                </CarouselItem>)}
+            </CarouselContent>
+        </Carousel>
     )
 }
 
---
+++ src/components/work-section-card.tsx
@@ -0,0 +1,34 @@
+import { useTheme } from "./theme-provider";
+import { Button } from "./ui/button";
+import { scrollToSectionMiddle } from "@/lib/utils";
+
+interface WorkSectionCardProps {
+    title: string;
+    description: string;
+    image?: any;
+    siteLink?: string;
+}
+
+const WorkSectionCard = ({ title, description, image, siteLink }: WorkSectionCardProps) => {
+    const { theme } = useTheme();
+
+    return (
+        <div className="flex flex-col justify-between h-full items-center">
+            <div className="flex flex-col items-center gap-3">
+                <div className={"text-2xl".concat(theme == 'dark' ? ' text-purple-400' : ' text-blue-400')}>{title}</div>
+                <div className="text-center">{description}</div>
+            </div>
+            {image
+                ? <img src={image} />
+                : <div className={`${theme == 'light' ? 'bg-gray-500' : 'bg-blue-950'} h-[40%] w-full border rounded-md flex justify-center items-center text-lg`}>
+                    ...
+                </div>}
+            <div className="flex gap-3">
+                <Button onClick={() => { if (Boolean(siteLink)) { window.open(siteLink, '_blank') } }}>Live Demo</Button>
+                <Button onClick={() => scrollToSectionMiddle("contact")} className="bg-gray-700 text-gray-300 hover:bg-gray-600 hover:text-gray-200">Make your own</Button>
+            </div>
+        </div >
+    )
+}
+
+export default WorkSectionCard

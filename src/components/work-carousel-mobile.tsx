0685d33fd234b72ddd1e6e7c0c5ab43aec074768
MorneNemdil <mornenemdilbusiness@gmail.com>
2025-07-07 14:23:36 +0100

    added swipe animation for work section

--- src/components/work-carousel-mobile.tsx
+++ src/components/work-carousel-mobile.tsx
@@ -1,6 +1,8 @@
 import type { JSX } from "react";
 import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 import { Card } from "./ui/card";
+import "@/my-css.css"
+import SwipeLeftIndicator from "./swipe-left-indicator";
 
 type WorkCarouselMobileProps = {
     slides: { index: number, content: JSX.Element }[];
@@ -8,13 +10,16 @@ type WorkCarouselMobileProps = {
 
 const WorkCarouselMobile = ({ slides }: WorkCarouselMobileProps) => {
     return (
-        <Carousel className="w-[70vw]">
-            <CarouselContent>
-                {slides.map(slide => <CarouselItem className="!h-[50vh] md:!h-[60vh] lg:!h-[70vh]" key={slide.index}>
-                    <Card className="h-full">{slide.content}</Card>
-                </CarouselItem>)}
-            </CarouselContent>
-        </Carousel>
+        <div className="flex flex-col items-center gap-8">
+            <Carousel className="w-[70vw]">
+                <CarouselContent>
+                    {slides.map(slide => <CarouselItem className="!h-[50vh] md:!h-[60vh] lg:!h-[70vh]" key={slide.index}>
+                        <Card className="h-full">{slide.content}</Card>
+                    </CarouselItem>)}
+                </CarouselContent>
+            </Carousel>
+            <SwipeLeftIndicator />
+        </div>
     )
 }
 

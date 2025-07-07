841c714c497532a800575a8e404bf74332014da5
MorneNemdil <mornenemdilbusiness@gmail.com>
2025-07-07 14:23:36 +0100

    added swipe animation for work section

---
+++ src/components/swipe-left-indicator.tsx
@@ -0,0 +1,11 @@
+import "@/my-css.css";
+
+const SwipeLeftIndicator = () => {
+    return (
+        <div className="w-16 h-8 border-3 rounded-full border-gray-700">
+            <div className="swipe-left" />
+        </div>
+    )
+}
+
+export default SwipeLeftIndicator

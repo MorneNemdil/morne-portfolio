c0c1c45a1ffb935afa2ae2f71dff8352851178a8
MorneNemdil <mornenemdilbusiness@gmail.com>
2025-10-04 00:08:43 +0100

    added modal to enter email

--- src/components/lucide-button.tsx
+++ src/components/lucide-button.tsx
@@ -9,11 +9,11 @@ const LucideButton = ({ Icon, onClick }: {
     const { theme } = useTheme()
 
     return (
-        <div className={`p-1 pb-1.5 border rounded-full hover:shadow hover:shadow-gray-700 hover:cursor-pointer w-min 
+        <div className={`p-1 pb-1.5 shadow-xl border rounded-full hover:shadow hover:shadow-gray-700 hover:cursor-pointer w-min 
             ${theme == 'dark' && "bg-white text-black"}`}
             onClick={onClick}
         >
-            <Icon />
+            <Icon className='mt-0.5 mx-0.5' />
         </div>
     )
 }
